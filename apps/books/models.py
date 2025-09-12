e19021de5378b4444ac4fd0721b23e113c730fb2
Ian Murzac <ianik123450909w@gmail.com>
2025-09-13 00:30:26 +0300

    feat(books): add Book model
    - added fields: title, subtitle, description, published_date, isbn
    - isbn validation implemented via pydantic-extra-types
    - inherited from BaseModel with created_at and updated_at

---
+++ apps/books/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+from apps.books.validators import validate_isbn
+from apps.common.models import BaseModel
+
+
+class Book(BaseModel):
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+    published_date = models.DateField(null=True, blank=True)
+    isbn = models.CharField(
+        max_length=13,
+        unique=True,
+        null=True,
+        blank=True,
+        validators=[validate_isbn],
+    )
