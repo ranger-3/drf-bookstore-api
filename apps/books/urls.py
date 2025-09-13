d254b0cecdab93faf8f46432d6f8cd41b1796983
Ian Murzac <ianik123450909w@gmail.com>
2025-09-13 22:52:27 +0300

    test: add API tests for books

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -3,8 +3,10 @@ from rest_framework.routers import DefaultRouter
 
 from apps.books.views import BookViewSet
 
+app_name = "books"
+
 router = DefaultRouter()
-router.register(r"books", BookViewSet, basename="books")
+router.register(r"books", BookViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
---
+++ bookstore/test_settings.py
@@ -0,0 +1,8 @@
+from bookstore.settings import *  # noqa: F403
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
