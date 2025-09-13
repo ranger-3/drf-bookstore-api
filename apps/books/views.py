35a29802916767cf46ed9f684af9402077dc7c2c
Ian Murzac <ianik123450909w@gmail.com>
2025-09-13 03:19:09 +0300

    feat(common): introduce BaseViewSet and refactor viewsets

--- apps/books/views.py
+++ apps/books/views.py
@@ -2,8 +2,9 @@ from rest_framework.viewsets import ModelViewSet
 
 from apps.books.models import Book
 from apps.books.serializers import BookSerializer
+from apps.common.views import BaseViewSet
 
 
-class BookViewSet(ModelViewSet):
+class BookViewSet(BaseViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
